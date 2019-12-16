8a8c402c5af78d675c4b20c96254c68787bcb810
Karun Sekhar <31802738+karsekh21@users.noreply.github.com>
2019-12-16 03:03:57 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
-const express = require("express");
-let app = express();
-let configRoutes = require("./routes");
-const bodyParser = require("body-parser");
+const express = require('express');
+const request = require('request');
+const app = express();
+const scribble = require('scribbletune');
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
+app.use(require("body-parser").json());
 
-app.get('/express', (req, res) => {
-  res.send({ express: 'Express backend is now connected to React app' });
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// create a GET route
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
 
-configRoutes(app);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.listen(port,() => {
-    console.log("We've now got a server!");
-    console.log(`Your routes will be running on port ${port}`);
+app.post('/download_midi', (req, res) => {
+    let allINeed = req.body.content;
+    let name = req.body.file_name;
+    console.log(allINeed);
+
+    request(
+        { url: 'http://localhost:5000/download_midi' },
+        (error, response, body) => {
+          scribble.midi(allINeed, name);
+          // console.log(allINeed);
+        }
+      )
+
 });
