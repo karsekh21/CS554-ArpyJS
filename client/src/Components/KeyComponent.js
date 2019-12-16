bfe3876e66b5f545d2e0c59c7f888eede3e35521
La-lit <Lalit.kargutkar@gmail.com>
2019-12-16 13:33:26 -0500

    Update KeyComponent.js

--- client/src/Components/KeyComponent.js
+++ client/src/Components/KeyComponent.js
@@ -9,7 +9,7 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import scribble from 'scribbletune';
 
-import './key.css';
+
 import Container from 'react-bootstrap/Container';
 // import axios from 'axios';S
 
